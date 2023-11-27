84b518f746ca0a517877519a262d15abf7e34dfb
Gerson Santos <gersones@outlook.com>
2023-11-27 20:50:15 -0300

    a

--- conexao.php
+++ conexao.php
@@ -7,6 +7,8 @@ $dbname="dbase";
 $conexao=mysqli_connect($servidor, $usuario, $senha, $dbname);
 if(!$conexao){
     die(("Houve um erro : ".mysqli_connect_errno()));
+}else {
+    echo "deu certo";
 }
 
 ?>
