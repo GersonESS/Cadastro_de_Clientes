77e54b8023f6645428a7544c4a056235d4ddeba4
Gerson Santos <gersones@outlook.com>
2023-11-28 03:23:38 -0300

    a

--- conexao.php
+++ conexao.php
@@ -3,7 +3,7 @@
 $servidor="localhost";
 $usuario="root";
 $senha="";
-$dbname="arqcli";
+$dbname="dbase";
 $conexao=mysqli_connect($servidor, $usuario, $senha, $dbname);
 if(!$conexao){
     die(("Houve um erro : ".mysqli_connect_errno()));
